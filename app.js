4d65a842385e0d0d3ceea3b7fad0b0c9a27262c1
Aiko-sys <aikoopersonal@gmail.com>
2024-08-04 19:52:44 -0300

    add server.js

--- app.js
+++ app.js
@@ -4,13 +4,4 @@ var app = express();
 
 app.use('/', indexRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error');
-});
-
 module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./app.js")
+
+
+app.listen(3000, () =>{
+    console.log("API RUNNING ON http://localhost:3000")
+})
